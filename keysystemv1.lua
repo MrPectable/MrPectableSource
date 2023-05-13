80770293537ff4c6e32065ede7f82d5473050891
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-05-13 00:03:20 -0400

    Update keysystemv1.lua

--- keysystemv1.lua
+++ keysystemv1.lua
@@ -1,6 +1,6 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Player = game.Players.localPlayer
-local Window = OrionLib:MakeWindow({Name = "MrPectable Key System", HidePremium = false, SaveConfig = true,IntroText = "Key System V1"})
+local Window = OrionLib:MakeWindow({Name = "MrPectable Key System", HidePremium = false, SaveConfig = true})
 
 OrionLib:MakeNotification({
 Name = "Logged in!",
