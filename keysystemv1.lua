7fb6bf475c6f9b66e2dd37ec43902f2a0e265c2e
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-05-12 21:59:14 -0400

    Update keysystemv1.lua

--- keysystemv1.lua
+++ keysystemv1.lua
@@ -8,7 +8,7 @@ Content = "You Are Logged in As"..Player.Name..".",
 Image = "http://www.roblox.com/thumbs/asset.ashx?assetid=13146328300&x=100&y=100&format=png",
 Time = 5
 })
-_G.Key = "OMG12"
+_G.Key = "GOA"
 _G.Key2 = "Hydrogen"
 _G.KeyInput = "string"
 
