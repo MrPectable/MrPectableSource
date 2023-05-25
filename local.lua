0c63ac746f2cf2365aeca7a78b3e5e46b98749fd
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-05-24 20:11:30 -0400

    Update local.lua

--- local.lua
+++ local.lua
@@ -1,9 +1,254 @@
-if game.PlaceId==() 
-  
-  then loadstring(game:HttpGet("scriptlink"))() 
+----- Pet Sim x--------
+if game.PlaceId==(6284583030) 
 
-else
-  
-end
+    then 
 
 
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+--------Blox Fruits----
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+  ----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+    ----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+    ----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+    ----------------------
+  if game.PlaceId==() 
+
+    then 
+
+
+  else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))() 
+  end
+ 
