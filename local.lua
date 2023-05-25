87e710ceb8522bf0c82ff64562a76688e986aa3c
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-05-25 11:59:46 -0400

    Update local.lua

--- local.lua
+++ local.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 6284583030 then -- psx
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/psx.lua", true))()
     else if game.PlaceId == 4738545896 then -- bloxfruits
     
         else if game.PlaceId == 746820961 then -- bloxburg
