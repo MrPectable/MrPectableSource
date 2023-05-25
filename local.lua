69a9b955a3a981e84793e74d5009871a2c7985d1
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-05-25 12:42:23 -0400

    Update local.lua

--- local.lua
+++ local.lua
@@ -1,37 +1,37 @@
 if game.PlaceId == 6284583030 then -- psx
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/psx.lua", true))()
     else if game.PlaceId == 2753915549 then -- bloxfruits
-    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/bloxfruits.lua", true))()
         else if game.PlaceId == 185655149 then -- bloxburg
-           
+           loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/bloxburg.lua", true))()
             else if game.PlaceId == 4872321990 then -- islands
-               
+               loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/islands.lua", true))()
                 else if game.PlaceId == 142823291 then -- mm2
-                   
+                   loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mm2.lua", true))
                     else if game.PlaceId == 286090429 then -- arsenal
-                        
+                        loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/arsenal.lua", true))()
                         else if game.PlaceId == 6516141723 then -- doors
-                            
+                            loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/doors.lua", true))()
                             else if game.PlaceId == 9872472334 then -- evade 
-                                
+                                loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/evade.lua", true))()
                                 else if game.PlaceId == 4282985734 then -- combat warriors
-                                    
+                                    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/combatwarriors.lua", true))()
                                     else if game.PlaceId == 9476339275 then -- double down
-                                        
+                                        loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/doubledown.lua", true))()
                                         else if game.PlaceId == 2788229376 then -- dahood
-                                            
-                                            else if game.PlaceId == 920587237 then -- adpot me
-                                                
+                                            loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/dahood.lua", true))()
+                                            else if game.PlaceId == 920587237 then -- adopt me
+                                                loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/adoptme.lua", true))()
                                                 else if game.PlaceId == 4924922222 then -- brookhaven
-                                                    
+                                                    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/brookhaven.lua", true))()
                                                     else if game.PlaceId == 370731277 then -- meepcity
-                                                        
+                                                        loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/meepcity.lua", true))()
                                                         else if game.PlaceId == 537413528 then -- build a boat
-                                                            
+                                                            loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/buildaboat.lua", true))()
                                                             else if game.PlaceId == 893973440 then -- FTF
-                                                                
+                                                                loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/FTF.lua", true))()
                                                                 else if game.PlaceId == 6872265039 then -- Bedwars
-                                                                    
+                                                                    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/bedwars.lua", true))()
                     else 
                     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))()
                 end
