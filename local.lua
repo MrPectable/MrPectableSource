8692d7dd9475e3815100e326be2a59f07b9b9264
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-05-25 12:38:18 -0400

    Update local.lua

--- local.lua
+++ local.lua
@@ -1,13 +1,37 @@
 if game.PlaceId == 6284583030 then -- psx
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/psx.lua", true))()
-    else if game.PlaceId == 4738545896 then -- bloxfruits
+    else if game.PlaceId == 2753915549 then -- bloxfruits
     
-        else if game.PlaceId == 746820961 then -- bloxburg
+        else if game.PlaceId == 185655149 then -- bloxburg
            
-            else if game.PlaceId == 413424176 then -- islands
+            else if game.PlaceId == 4872321990 then -- islands
                
-                else if game.PlaceId == 6717202874 then -- mm2
+                else if game.PlaceId == 142823291 then -- mm2
                    
+                    else if game.PlaceId == 286090429 then -- arsenal
+                        
+                        else if game.PlaceId == 6516141723 then -- doors
+                            
+                            else if game.PlaceId == 9872472334 then -- evade 
+                                
+                                else if game.PlaceId == 4282985734 then -- combat warriors
+                                    
+                                    else if game.PlaceId == 9476339275 then -- double down
+                                        
+                                        else if game.PlaceId == 2788229376 then -- dahood
+                                            
+                                            else if game.PlaceId == 920587237 then -- adpot me
+                                                
+                                                else if game.PlaceId == 4924922222 then -- brookhaven
+                                                    
+                                                    else if game.PlaceId == 370731277 then -- meepcity
+                                                        
+                                                        else if game.PlaceId == 537413528 then -- build a boat
+                                                            
+                                                            else if game.PlaceId == 893973440 then -- FTF
+                                                                
+                                                                else if game.PlaceId == 6872265039 then -- Bedwars
+                                                                    
                     else 
                     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))()
                 end
