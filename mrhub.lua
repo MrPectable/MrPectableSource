a6ac8c305dc83cffffad392bb82d0bf352ae65e4
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-05-29 16:33:23 -0400

    Update mrhub.lua

--- mrhub.lua
+++ mrhub.lua
@@ -1,11 +1,11 @@
-if game.PlaceId == 6284583030 then -- psx
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrpsx.lua", true))()
-    else if game.PlaceId == 2753915549 then -- bloxfruits
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrfruits.lua", true))()    
-                    else 
-                    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrhub2.lua"))()
-                end
-            end
-        end
-    end   
+if game.PlaceId == 6284583030 then 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrpsx.lua", true))()
+else if game.PlaceId == 2753915549 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrfruits.lua", true))()    
+else 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrhub2.lua"))()
+end
+end
+end
+end   
 end
