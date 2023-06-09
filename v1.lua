9779b5865c7b3ba7563bf88cf8fc36fc722d1623
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-06-09 04:19:58 -0400

    Update v1.lua

--- v1.lua
+++ v1.lua
@@ -140,7 +140,7 @@ local ASSET_ID = txt
 
 local asset = MarketplaceService:GetProductInfo(ASSET_ID)
       
-
+print(asset.Name .. " :: " .. asset.Description .. asset.IsLimited)
 
        warn(txt)
    end
