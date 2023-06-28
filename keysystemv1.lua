b92fa04f7e17852a0ed7cc64d6f541749eaafc17
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-06-28 01:29:32 -0400

    Update keysystemv1.lua

--- keysystemv1.lua
+++ keysystemv1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua')))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/local.lua')))()
