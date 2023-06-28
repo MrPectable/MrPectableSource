5bbfc455c4861326f155e2b61fb6e15fc3274f79
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-06-28 03:08:22 -0400

    Update mrpsx.lua

--- mrpsx.lua
+++ mrpsx.lua
@@ -45,6 +45,7 @@ Text = "Loaded Successfully";
 end)
 
 Home:addLabel("Last Updated June 28 2023 EST|","Any Issues Contact MrPectable#2662 Discord")
+Home:addLabel("Discord","https://discord.gg/zuPeYVchv3")
 
 
 
