a1ed87300e5dd66670e8944d061fb5fc6502b300
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-06-28 02:14:00 -0400

    Update local.lua

--- local.lua
+++ local.lua
@@ -1,11 +1,8 @@
 if game.PlaceId == 6284583030 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrpsx.lua", true))()
-    else if game.PlaceId == 2753915549 then
+    else 
+if game.PlaceId == 2753915549 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrfruits.lua", true))()
                     else 
                     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/v1.lua"))()
-                end
-            end
-        end
-    end   
-end
+    end
