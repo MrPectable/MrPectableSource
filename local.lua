3961a136b1948d06504264a602ca2fdfea493efe
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-06-29 02:47:38 -0400

    Update local.lua

--- local.lua
+++ local.lua
@@ -3,6 +3,37 @@ if (gameid == 6284583030) then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrpsx.lua", true))()
     else if (gameid == 2753915549) then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrfruits.lua", true))()
+        else if (gameid == 185655149) then 
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrburg.lua", true))()     
+            else if (gameid == 4872321990) then 
+               loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrislands.lua", true))()
+                else if (gameid == 142823291) then 
+                   loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrmm2.lua", true))
+                    else if (gameid == 286090429) then 
+                        loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrarsenal.lua", true))()
+                        else if (gameid == 6516141723) then 
+                            loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrdoors.lua", true))()
+                            else if (gameid == 9872472334 )then  
+                                loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrevade.lua", true))()
+                                else if (gameid == 4282985734) then 
+                                    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrcombatwarriors.lua", true))()
+                                    else if (gameid  == 9476339275) then 
+                                        loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrdoubledown.lua", true))()
+                                        else if (gameid == 2788229376) then 
+                                            loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrdahood.lua", true))()
+                                            else if (gameid == 920587237) then 
+                                                loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mradoptme.lua", true))()
+                                                else if (gameid == 4924922222) then 
+                                                    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrbrookhaven.lua", true))()
+                                                    else if (gameid == 370731277) then 
+                                                        loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrmeepcity.lua", true))()
+                                                        else if (gameid == 537413528) then 
+                                                            loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrbuildaboat.lua", true))()
+                                                            else if (gameid == 893973440) then 
+                                                                loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrFTF.lua", true))()
+                                                                else if (gameid == 6872265039) then 
+                                                                    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrbedwars.lua", true))()
+                   
                     else 
      loadstring(game:HttpGet("https://raw.githubusercontent.com/MrPectable/MrPectableSource/main/mrhub.lua"))()
     end
