50c7fa0baff4e30861c37d62d96128145591b0ba
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-06-29 03:17:41 -0400

    Create mradoptme.lua

---
+++ mradoptme.lua
@@ -0,0 +1,9 @@
+LP:addLabel("Adopt Me","Soon")
+
+LP:addToggle("V.G HUB",function(value)
+    if value == false then
+    
+    else
+      loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
+    end
+end)
