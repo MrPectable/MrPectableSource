34800cdafa3c0379a4308b07fda13cf394ab96c8
MrPectable <126442140+MrPectable@users.noreply.github.com>
2023-07-03 07:06:21 -0400

    Update mrarsenal.lua

--- mrarsenal.lua
+++ mrarsenal.lua
@@ -44,7 +44,7 @@ Text = "Loaded Successfully";
 })
 end)
 
-Home:addLabel("Last Updated June 28 2023 EST|","Any Issues Contact MrPectable#2662 Discord")
+Home:addLabel("Last Updated July 3rd 2023 EST|","Any Issues Contact MrPectable#2662 Discord")
 Home:addLabel("Discord","https://discord.gg/zuPeYVchv3")
 
 
